030978f84a60d8f32fa8c822927700cdd82145b2
Kiki <kirstinlisnoff5@gmail.com>
2025-01-21 19:13:32 -0500

    worked on blog js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,11 +1,53 @@
 // TODO: Create a variable that selects the main element, and a variable that selects the back button element
 
+const mainElement = document.querySelector("main");
+const backButton = document.getElementById("back");
+
 // TODO: Create a function that builds an element and appends it to the DOM
+function newElement(tag, attributes, parent) {
+    const element = document.createElement(tag);
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+    parent.appendChild(element);
+};
 
 // TODO: Create a function that handles the case where there are no blog posts to display
 
+function noPosts () {
+    const mainElement = document.querySelector("main");
+    const message = document.createElement("p");
+    message.textContent = "No blog posts yet...";
+    mainElement.appendChild(message);
+}
+ 
 // TODO: Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
+
+function renderBlogList() {
+    const posts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+    if (posts.length===0) {
+        noPosts();
+    }else{
+        posts.array.forEach(post => {
+            const article = document.createElement("article");
+            article.className = "card";
+            article.innerHTML = `
+            <h2>${post.title}</h2>
+            <blockquote>${post.content}</blockquote>
+            <p>Posted by: ${post.username}<p>
+            `;
+    mainElement.appendChild(article);
+        });
+    }
+ }
+
 
 // TODO: Call the `renderBlogList` function
 
+renderBlogList();
+
 // TODO: Redirect to the home page using the `redirectPage` function found in logic.js when the back button is clicked
+
+backButton.addEventListener('click', ()=> {
+    redirectPage('./index.html');
+})
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,29 +1,29 @@
 // TODO: Create a variable that selects the form element
 
 const formElement = document.getElementById("form");
+const errorElement = document.getElementById("error");
 
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
 
 
-const storingBlog = function (event){
+function storingBlog (event){
     event.preventDefault();
-    const username = document.getElementById("username").value;
-    const title = document.getElementById("title").value;
-    const content = document.getElementById("content").value;
-    const error = document.getElementById("error");
+    const username = document.getElementById("username").value.trim();
+    const title = document.getElementById("title").value.trim();
+    const content = document.getElementById("content").value.trim();
+
 if (!username || !title || !content) {
-    error.textContent = "Please complete the form.";
-    error.style.display = "block";
+    errorElement.textContent = "Please complete the form.";
     return;
 }
 
 const formInfo = {
     username: username,
     title: title,
-    content: content
+    content: content,
 };
 
-localStorage.setItem("formInfo", JSON.stringify(formInfo));
+storeLocalStorage('blogPosts', formInfo);
 redirectPage("./blog.html");
 };
 
